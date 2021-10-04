84ec125f48182c4df88a523d5990e6ea3666a09f
Kamindu99 <kamidugayantha123@gmail.com>
2021-10-05 05:24:13 +0530

    Change Rating model

--- models/TravelPackageRating.js
+++ models/TravelPackageRating.js
@@ -8,8 +8,6 @@ const reviewSchema = new mongoose.Schema(
       required: true,
     },
 
-    name: { type: String, required: true },
-    comment: { type: String, required: true },
     rating: { type: Number, required: true },
   },
   {
